783c84351773a211d232355230a546547bee0054
thunderchunk <lucaswhawk@gmail.com>
2017-01-22 12:49:44 -0600

    menus and modals complete

--- public/app/views/nav/nav.js
+++ public/app/views/nav/nav.js
@@ -5,7 +5,23 @@ angular.module('bash')
             templateUrl: './app/views/nav/nav.html',
             link: function(scope, element, attributes) {
                 
-                
+              $(document).ready(function(){
+              $(function() {
+                // Select link by id and add click event
+                $('#redcap').click(function() {
+
+                  // Animate Scroll to #bottom
+                  $('html,body').animate({
+                    scrollTop: $("#mainNav").offset().top }, // Tell it to scroll to the top #bottom
+                    10 // How long scroll will take in milliseconds
+                  );
+
+                  // Prevent default behavior of link
+                  return false;
+                });
+              });
+              
+            });
 
 
             }
